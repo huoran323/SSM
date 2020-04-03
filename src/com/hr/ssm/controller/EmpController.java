0e6f1725ac07b23f4284898062be213c8f72b80e
xiaoran <303176152@qq.com>
2020-04-03 14:41:33 +0800

    SSM搭建完成

--- src/com/hr/ssm/controller/EmpController.java
+++ src/com/hr/ssm/controller/EmpController.java
@@ -1,8 +1,13 @@
 package com.hr.ssm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.hr.ssm.service.EmpService;
 
 @Controller
 public class EmpController {
 
+	@Autowired
+	private EmpService empService; 
 }
--- src/com/hr/ssm/service/impl/EmpServiceImpl.java
+++ src/com/hr/ssm/service/impl/EmpServiceImpl.java
@@ -1,10 +1,15 @@
 package com.hr.ssm.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hr.ssm.mapper.EmpMapper;
 import com.hr.ssm.service.EmpService;
 
 @Service
 public class EmpServiceImpl implements EmpService {
 
+	@Autowired
+	private EmpMapper mapper;
+	
 }
