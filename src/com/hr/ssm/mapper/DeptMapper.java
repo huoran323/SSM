a1afcdb15b56be305fcf43406885446120eb6181
xiaoran <303176152@qq.com>
2020-04-07 16:45:03 +0800

    commit

---
+++ src/com/hr/ssm/mapper/DeptMapper.java
@@ -0,0 +1,6 @@
+package com.hr.ssm.mapper;
+
+public interface DeptMapper {
+
+	//获取所有的部门信息
+}
--- src/com/hr/ssm/mapper/EmpMapper.java
+++ src/com/hr/ssm/mapper/EmpMapper.java
@@ -1,5 +1,17 @@
 package com.hr.ssm.mapper;
 
+import java.util.List;
+
+import com.hr.ssm.bean.Emp;
+
 public interface EmpMapper {
 
+	//获取所有的员工信息
+	List<Emp> getAllEmp();
+	
+	//根据eid获取员工信息
+	Emp getEmpByEid(String eid);
+	
+	//修改员工信息
+	void updateEmp(Emp emp);
 }
