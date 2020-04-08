dfa6140fe08a47118c381e2e5244f6e5fd6e5f9a
xiaoran <303176152@qq.com>
2020-04-08 09:53:03 +0800

    commit

--- src/com/hr/ssm/controller/EmpController.java
+++ src/com/hr/ssm/controller/EmpController.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hr.ssm.bean.Emp;
 import com.hr.ssm.service.EmpService;
 
@@ -17,10 +20,12 @@ public class EmpController {
 	@Autowired
 	private EmpService empService; 
 	
-	@RequestMapping(value="/emps", method=RequestMethod.GET)
-	public String getAllEmp(Map<String, Object> map) {
+	@RequestMapping(value="/emps/{pageNum}", method=RequestMethod.GET)
+	public String getAllEmp(Map<String, Object> map, @PathVariable("pageNum")Integer pageNum) {
 		
+		PageHelper.startPage(pageNum, 2);
 		List<Emp> empList = empService.getAllEmp();
+		PageInfo<Emp> pageInfo = new PageInfo<>(empList, 5);
 		map.put("empList", empList);
 		return "list";
 	}
